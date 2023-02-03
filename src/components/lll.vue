0b81b819c4dfa77f9e6a88a2f22bad95d813636c
Dong-Yi-Xia <dong7777xia@gmail.com>
2023-02-03 13:35:44 -0800

    first commit

---
+++ src/components/lll.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>hello world</div>
+</template>
+
+<script>
+export default {
+  name: "FirstComponent",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  props: {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
